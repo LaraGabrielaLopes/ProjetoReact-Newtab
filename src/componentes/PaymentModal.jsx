a1dc479fe77fc6d556c71d9a9eae82a0152ca00a
Lara Gabriela <lara_g_lopes@hotmail.com>
2023-08-07 16:04:42 -0300

    userID no payload e finalização do projeto

--- src/componentes/PaymentModal.jsx
+++ src/componentes/PaymentModal.jsx
@@ -5,8 +5,6 @@ import axios from "axios";
 export default function PaymentModal(props) {
 
     //Definindo estado inicial:
-    const [userID, setUserID] = useState("");
-    const [paymentValue, setPaymentValue] = useState("");
     const [cardInfo, setCardInfo] = useState({});
     const [paymentValueFloat, setPaymentValueFloat] = useState(0);
 
@@ -60,109 +58,111 @@ export default function PaymentModal(props) {
         })
       }
 
-      setPaymentValue(e.target.value)
-      let valueFloat = (e.target.value.replaceAll('R$', '').replaceAll('.', '').replaceAll(',', '.'))
+      let setPaymentValue = (e.target.value)
+      let valueFloat = (setPaymentValue.replaceAll('R$', '').replaceAll('.', '').replaceAll(',', '.'))
       console.log(valueFloat)
       setPaymentValueFloat(parseFloat(valueFloat))
     }
 
+    //Fazendo a requisição: 
     const POSTObject = {
         cardInfo,
-        userID,
+        userID: props.userID,
         paymentValueFloat,
       };
       
-      const submitHandler = (e) => {
-        e.preventDefault();
+    // Validação do formulário
+    const submitHandler = (e) => {
+      e.preventDefault();
 
-        if (!cardInfo.card_number) {
-          alert("Por favor, selecione um cartão antes de fazer o pagamento.");
-          return;
-        }
-      
-        axios
-          .post(
-            "https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989",
-            POSTObject
-          )
-          .then((response) => {
-            console.log(response);
-            
-            if (response.data.status === "Aprovada" && cardInfo.card_number === "1111111111111111") {
-              props.setMessage("O pagamento foi concluído com sucesso!");
-            } else if (response.data.status === "Aprovada" && cardInfo.card_number === "4111111111111234") {
-              props.setMessage("O pagamento NÃO foi concluído com sucesso");
-            }
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      };
+      if (!cardInfo.card_number) {
+        alert("Por favor, selecione um cartão antes de fazer o pagamento.");
+        return;
+      }
     
-    return(
-        <div className="ModalBackground" id="modal" onClick={handleOutsideModal}>
-            {props.message === "" ? (
-            <div className="ModalContainer">
-                <div className="HeaderDiv">
-                    <header className="PaymentHeader">
-                    Pagamento para {props.name}
-                    </header>
-                    <button
-                    className="CloseButton"
-                    onClick={() => props.isClosed(false)}
-                    >
-                    &times;
-                    </button>
-                </div>
-                <form className="PaymentForm" onSubmit={submitHandler}>
-                    <input
-                    className="PaymentInput"
-                    placeholder="R$ 0,00"
-                    name="paymentValue"
-                    onKeyDown={moneyMaskTestFilter}
-                    onKeyUp={moneyMaskFormatValue}
-                    />
-                    <select
-                    className="PaymentSelect"
-                    name="cardInfo"
-                    defaultValue={"default"}
-                    onChange={(e) => {
-                        setCardInfo(JSON.parse(e.target.value));
-                    }}
-                    >
-                        <option value={"default"} disabled>
-                        Escolha um cartão para o pagamento
-                        </option>
-                        {cards.map((card) => (
-                        <option 
-                        key={card.card_number} 
-                        value={JSON.stringify(card)}>
-                        {card.card_number}
-                        </option>
-                    ))}
-                    </select>
-                    <div className="ButtonDiv">
-                    <button type="submit">Pagar</button>
-                    </div>
-                </form>
-            </div>
-      ) : (
-        <div className="ModalContainer">
-            <div className="HeaderDiv">
-                <header className="PaymentHeader">
-                Recibo de Pagamento
-                </header>
-                <button
-                className="CloseButton"
-                onClick={handleResetModal}
-                >
-                &times;
-                </button>
-            </div>
-            <p>{props.message}</p>
-        </div>
-      )}
-      ;
-    </div>
-  );
+      axios
+        .post(
+          "https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989",
+          POSTObject
+        )
+        .then((response) => {
+          console.log(response);
+          
+          if (response.data.status === "Aprovada" && cardInfo.card_number === "1111111111111111") {
+            props.setMessage("O pagamento foi concluído com sucesso!");
+          } else if (response.data.status === "Aprovada" && cardInfo.card_number === "4111111111111234") {
+            props.setMessage("O pagamento NÃO foi concluído com sucesso");
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+  
+  return(
+      <div className="ModalBackground" id="modal" onClick={handleOutsideModal}>
+          {props.message === "" ? (
+          <div className="ModalContainer">
+              <div className="HeaderDiv">
+                  <header className="PaymentHeader">
+                  Pagamento para {props.name}
+                  </header>
+                  <button
+                  className="CloseButton"
+                  onClick={() => props.isClosed(false)}
+                  >
+                  &times;
+                  </button>
+              </div>
+              <form className="PaymentForm" onSubmit={submitHandler}>
+                  <input
+                  className="PaymentInput"
+                  placeholder="R$ 0,00"
+                  name="paymentValue"
+                  onKeyDown={moneyMaskTestFilter}
+                  onKeyUp={moneyMaskFormatValue}
+                  />
+                  <select
+                  className="PaymentSelect"
+                  name="cardInfo"
+                  defaultValue={"default"}
+                  onChange={(e) => {
+                      setCardInfo(JSON.parse(e.target.value));
+                  }}
+                  >
+                      <option value={"default"} disabled>
+                      Escolha um cartão para o pagamento
+                      </option>
+                      {cards.map((card) => (
+                      <option 
+                      key={card.card_number} 
+                      value={JSON.stringify(card)}>
+                      {card.card_number}
+                      </option>
+                  ))}
+                  </select>
+                  <div className="ButtonDiv">
+                  <button type="submit">Pagar</button>
+                  </div>
+              </form>
+          </div>
+    ) : (
+      <div className="ModalContainer">
+          <div className="HeaderDiv">
+              <header className="PaymentHeader">
+              Recibo de Pagamento
+              </header>
+              <button
+              className="CloseButton"
+              onClick={handleResetModal}
+              >
+              &times;
+              </button>
+          </div>
+          <p>{props.message}</p>
+      </div>
+    )}
+    ;
+  </div>
+);
 }
